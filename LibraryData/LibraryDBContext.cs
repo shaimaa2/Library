259a2e0d0f21c1da5ad456319b5c12df937f35de
shaimaa <shaimaa@SHAIMAA-PC>
2018-02-07 13:32:16 +0200

    First add connectionString

---
+++ LibraryData/LibraryDBContext.cs
@@ -0,0 +1,11 @@
+﻿using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LibraryData
+{
+    public class LibraryDBContext : DbContext
+    {
+        public DbSet<Patron> patrons { get; set; }
+    }
+}
